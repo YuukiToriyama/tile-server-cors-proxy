ac853c712b78c932b257d88690e0156204ba321b
Yuuki Toriyama <github@toriyama.dev>
2023-06-25 18:22:11 +0900

    パスを追加

--- src/router.ts
+++ src/router.ts
@@ -14,4 +14,26 @@ router.get("/ritsumei-arc/meisai-sougou/:z/:x/:y", ({ params }) => {
 	})
 })
 
+router.get("/ritsumei-arc/meisai-hasegawa/:z/:x/:y", ({ params }) => {
+	const tileServerProxy = new TileServerProxy({
+		endpoint: 'https://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_hasegawa/{z}/{x}/{y}.png'
+	})
+	return tileServerProxy.getProxiedResponse({
+		x: params.x,
+		y: params.y,
+		z: params.z
+	})
+})
+
+router.get("/ritsumei-arc/kyoto-city-s28/:z/:x/:y", ({ params }) => {
+	const tileServerProxy = new TileServerProxy({
+		endpoint: 'https://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/s28/{z}/{x}/{y}.png'
+	})
+	return tileServerProxy.getProxiedResponse({
+		x: params.x,
+		y: params.y,
+		z: params.z
+	})
+})
+
 export default router
