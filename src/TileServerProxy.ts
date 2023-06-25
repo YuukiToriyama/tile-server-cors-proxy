f7f838bcd57345f882b9a114536bd94be3c56d59
Yuuki Toriyama <github@toriyama.dev>
2023-06-25 17:49:01 +0900

    レスポンスにCORSヘッダをつける処理をprivateメソッドとして切り出した

--- src/TileServerProxy.ts
+++ src/TileServerProxy.ts
@@ -10,9 +10,7 @@ export class TileServerProxy {
 	async getProxiedResponse(xyz: { x: string, y: string, z: string }): Promise<Response> {
 		const proxyUrl = this.getProxyUrl(xyz.x, xyz.y, xyz.z)
 		let response = await fetch(proxyUrl)
-		let proxiedResponse = new Response(response.body, response)
-		proxiedResponse.headers.set("Access-Control-Allow-Origin", "*")
-		return proxiedResponse
+		return this.appendCorsHeader(response, "*")
 	}
 
 	private getProxyUrl(x: string, y: string, z: string) {
@@ -20,5 +18,11 @@ export class TileServerProxy {
 			.replace("{z}", z)
 			.replace("{x}", x)
 			.replace("{y}", y)
+	}
+
+	private appendCorsHeader(response: Response, origin: string): Response {
+		const corsAppendedResponse = new Response(response.body, response)
+		corsAppendedResponse.headers.set("Access-Control-Allow-Origin", origin)
+		return corsAppendedResponse
 	}
 }
